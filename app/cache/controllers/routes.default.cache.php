823773271fc83c299908aa5bdedf5d9986154094
Gaetan <gaetanclb@gmail.com>
2022-03-24 15:19:10 +0100

    B fini et C fait à moitié

---
+++ app/cache/controllers/routes.default.cache.php
@@ -0,0 +1,2 @@
+<?php
+return array("/_default/"=>["controller"=>"controllers\\StoreController","action"=>"index","parameters"=>[],"name"=>"home","cache"=>false,"duration"=>0],"/store/section/(.+?)/"=>["controller"=>"controllers\\StoreController","action"=>"section","parameters"=>[0],"name"=>"store.section","cache"=>false,"duration"=>0]);
--- app/controllers/StoreController.php
+++ app/controllers/StoreController.php
@@ -1,6 +1,9 @@
 <?php
 namespace controllers;
+ use models\Product;
+ use models\Section;
  use Ubiquity\attributes\items\router\Route;
+ use Ubiquity\orm\DAO;
 
  /**
   * Controller StoreController
@@ -9,6 +12,13 @@ class StoreController extends \controllers\ControllerBase{
 
     #[Route('_default', name: 'home')]
 	public function index(){
-		$this->loadView("StoreController/index.html");
+        $sections = DAO::getAll(Section::class);
+		$this->loadView("StoreController/index.html",compact('sections'));
+	}
+
+    #[Route('store/section/{idSection}', name: 'store.section')]
+	public function section($idSection){
+        $section = DAO::getById(Section::class, $idSection);
+		$this->loadView('StoreController/section.html',compact('section'));
 	}
 }
